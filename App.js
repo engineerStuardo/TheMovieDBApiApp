b960537f26dd888607441f479b65428f70c0e16c
icortez <icortez@ufg.edu.sv>
2021-08-10 13:15:03 -0600

    work and start showing first popular movie

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
 
+import {getPopularMovies} from './services/services';
+
 const App = () => {
+  const [movie, setMovie] = useState('');
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getPopularMovies()
+      .then(movies => {
+        setMovie(movies[0]);
+        setError(false);
+      })
+      .catch(e => setError(e));
+  }, []);
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello world</Text>
+      <Text>Movie name: {movie.original_title}</Text>
+      <Text>Language: {movie.original_language}</Text>
+      <Text>Release Date: {movie.release_date}</Text>
+      {error && <Text style={{color: 'red'}}>Error in the server</Text>}
     </View>
   );
 };
