c8f5898f9347bd407e2cbc1fb08162a5bfaa6b87
icortez <icortez@ufg.edu.sv>
2021-08-11 16:20:36 -0600

    add image as a placeholder when it doesnt have an image and addint the title of the movie

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import {TouchableOpacity, Image} from 'react-native';
+import {Text, TouchableOpacity, Image} from 'react-native';
+
+const placeHolderImage = require('../assets/images/placeholder.png');
 
 const Card = ({item}) => {
   return (
-    <TouchableOpacity style={{padding: 5, position: 'relative'}}>
+    <TouchableOpacity
+      style={{
+        padding: 5,
+        position: 'relative',
+        alignItems: 'center',
+        height: 200,
+      }}>
       <Image
         resizeMode="cover"
         style={{height: 200, width: 120, borderRadius: 20}}
-        source={{uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`}}
+        source={
+          item.poster_path
+            ? {uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`}
+            : placeHolderImage
+        }
       />
+      {!item.poster_path && (
+        <Text
+          style={{
+            position: 'absolute',
+            width: 100,
+            textAlign: 'center',
+            bottom: 10,
+          }}>
+          {item.title}
+        </Text>
+      )}
     </TouchableOpacity>
   );
 };
