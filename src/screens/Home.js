e6b5889c2ed3a2bce746916249ee83c761bbd4c9
icortez <icortez@ufg.edu.sv>
2021-08-12 10:13:06 -0600

    add popular tv, family movies and documentary section

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,13 @@ import React, {useState, useEffect} from 'react';
 import {Text, View, Dimensions, FlatList, ScrollView} from 'react-native';
 import {SliderBox} from 'react-native-image-slider-box';
 
-import {getPopularMovies, getUpcomingMovies} from '../services/services';
+import {
+  getPopularMovies,
+  getUpcomingMovies,
+  getPopularTv,
+  getFamilyMovies,
+  getDocumentary,
+} from '../services/services';
 import List from '../components/List';
 
 const dimentions = Dimensions.get('screen');
@@ -10,6 +16,9 @@ const dimentions = Dimensions.get('screen');
 const Home = () => {
   const [movieImages, setMovieImages] = useState('');
   const [popularMovies, setPopularMovies] = useState('');
+  const [popularTv, setPopularTv] = useState('');
+  const [familyMovies, setFamilyMovies] = useState('');
+  const [documentary, setDocumentary] = useState('');
   const [error, setError] = useState(false);
 
   const upcomingMoviesRequest = async () => {
@@ -38,9 +47,42 @@ const Home = () => {
     }
   };
 
+  const popularTvRequest = async () => {
+    try {
+      const popularTv = await getPopularTv();
+      setPopularTv(popularTv);
+      setError(false);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const familyMoviesRequest = async () => {
+    try {
+      const familyMovies = await getFamilyMovies();
+      setFamilyMovies(familyMovies);
+      setError(false);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const documentaryRequest = async () => {
+    try {
+      const documentary = await getDocumentary();
+      setDocumentary(documentary);
+      setError(false);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   useEffect(() => {
     upcomingMoviesRequest();
     popularMoviesRequest();
+    popularTvRequest();
+    familyMoviesRequest();
+    documentaryRequest();
   }, []);
 
   return (
@@ -62,6 +104,15 @@ const Home = () => {
         </View>
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
           <List title="Popular Movies" content={popularMovies} />
+        </View>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <List title="Popular TV Shows" content={popularTv} />
+        </View>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <List title="Family Movies" content={familyMovies} />
+        </View>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <List title="Documentary" content={documentary} />
         </View>
       </ScrollView>
     </>
--- src/services/services.js
+++ src/services/services.js
@@ -4,22 +4,30 @@ const apiUrl = 'https://api.themoviedb.org/3';
 const apiKey = '949eb751d5ca24f1d34b3041669ce02f';
 
 export const getPopularMovies = async () => {
-  const resp = await axios.get(
-    `${apiUrl}/movie/popular?api_key=${apiKey}&language=en-US&page=1`,
-  );
+  const resp = await axios.get(`${apiUrl}/movie/popular?api_key=${apiKey}`);
   return resp.data.results;
 };
 
 export const getUpcomingMovies = async () => {
-  const resp = await axios.get(
-    `${apiUrl}/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`,
-  );
+  const resp = await axios.get(`${apiUrl}/movie/upcoming?api_key=${apiKey}`);
   return resp.data.results;
 };
 
 export const getPopularTv = async () => {
+  const resp = await axios.get(`${apiUrl}/tv/popular?api_key=${apiKey}`);
+  return resp.data.results;
+};
+
+export const getFamilyMovies = async () => {
   const resp = await axios.get(
-    `${apiUrl}/tv/popular?api_key=${apiKey}&language=en-US&page=1`,
+    `${apiUrl}/discover/movie?api_key=${apiKey}&with_genres=10751`,
+  );
+  return resp.data.results;
+};
+
+export const getDocumentary = async () => {
+  const resp = await axios.get(
+    `${apiUrl}/discover/movie?api_key=${apiKey}&with_genres=99`,
   );
   return resp.data.results;
 };
