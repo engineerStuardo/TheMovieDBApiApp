65744417486c7c896acec9c2257500462368c968
icortez <icortez@ufg.edu.sv>
2021-09-01 15:38:33 -0600

    add input and icon and make some styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export const Search = ({navigation}) => (
-  <View>
-    <Text>hello</Text>
-  </View>
-);
+export const Search = ({navigation}) => {
+  const [text, setText] = useState();
+
+  const onsubmit = () => {
+    console.log(text);
+  };
+
+  return (
+    <View
+      style={{
+        padding: 10,
+        paddingTop: 60,
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}>
+      <View style={{flexBasis: 'auto', flexGrow: 1, paddingRight: 8}}>
+        <TextInput
+          style={{height: 50, padding: 8, borderWidth: 0.5, borderRadius: 15}}
+          onChangeText={text => setText(text)}
+          value={text}
+          placeholder={'Search movie or TV Show'}
+        />
+      </View>
+      <TouchableOpacity onPress={() => onsubmit()}>
+        <Icon name={'search-outline'} size={30} color={'#000'} />
+      </TouchableOpacity>
+    </View>
+  );
+};
